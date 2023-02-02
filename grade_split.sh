c4a7caa308c0f09330410c233fa2539b7b96ba65
Joseph Caluya <jcaluya@usc.edu>
2023-02-02 05:18:52 +0000

    done with part 2

--- grade_split.sh
+++ grade_split.sh
@@ -1,14 +1,24 @@
-g++ split.cpp grade_split.cpp -o grade_split `pkg-config --cflags --libs gtest`
 TOTAL=12
+g++ split.cpp grade_split.cpp -o grade_split `pkg-config --cflags --libs gtest`
 retVal=$?
-    if [ $retVal -eq 0 ]
-    then
-        echo "grade_split compiled."
-    else
-		echo "grade_split did not compile. Make sure your split.cpp compiles with your test code."
+if [ $retVal -eq 0 ]
+then
+    echo "grade_split compiled."
+else
+    echo "grade_split did not compile. Make sure your split.cpp compiles with your test code."
 		echo "AUTO:T$TOTAL:P0:V0:0.0"
-		return -1
-    fi
+		exit -1
+fi
+g++ -E split.cpp |grep -q "for\|while"
+retVal=$?
+if [ $retVal -ne 0 ]
+then
+    echo "split.cpp contains no loops..."
+else
+    echo "split.cpp appears to contain loops. All code in split.cpp must be recursive."
+    echo "AUTO:T$TOTAL:P0:V0:0.0"
+    exit -1
+fi
 PASSED=0
 FAILED=0
 FAILED_TESTS=""
@@ -31,7 +41,7 @@ VFAILED_TESTS=""
 for test in Split.Empty Split.InputSetToNull Split.OneEven Split.OneOdd Split.TwoNumbers Split.Zero Split.AllOddsOneEven Split.TenNumbers Split.AllEvens Split.AllOdds Split.Duplicates Split.DuplicatesMixed
 do
     echo $test
-    valgrind --tool=memcheck --leak-check=yes ./grade_split --gtest_filter=$test
+    valgrind --error-exitcode=2 --tool=memcheck --leak-check=yes ./grade_split --gtest_filter=$test
     retVal=$?
     if [ $retVal -eq 0 ]
     then
--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -9,5 +9,23 @@
 
 int main(int argc, char* argv[])
 {
-
+	ULListStr dat;
+	dat.push_back("7");
+	dat.push_front("8");
+	dat.push_back("9");
+	dat.push_back("9");
+	dat.push_back("9");
+	dat.push_back("9");
+	dat.push_back("9");
+	dat.push_back("9");
+	dat.push_back("9");
+	dat.push_back("9");
+	dat.push_back("9");
+	dat.push_back("1");
+	std::cout << dat.get(0) << " " << dat.get(1) << " " << dat.get(2) << " " <<
+	dat.get(3) << " " << dat.get(4) << " " << dat.get(5) << " " <<
+	dat.get(6) << " " << dat.get(7) << " " << dat.get(8) <<  " " <<
+	dat.get(9) << " " << dat.get(10) << " " << dat.get(10) << std::endl;
+	// prints: 8 7 9
+	std::cout << dat.size() << std::endl;  // prints 3 since there are 3 strings stored
 }
